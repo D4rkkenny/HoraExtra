af6539537bebe4fcce618e47c9be1aa2c5e5d901
Bruno Monteiro <111515772+D4rkkenny@users.noreply.github.com>
2024-04-08 15:00:13 -0300

    atualização do projeto

---
+++ HoraExtra/HoraExtra.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace HoraExtra
+{
+    internal class HoraExtra
+    {
+        public TimeSpan[] escalaTrabalho { get; set; }
+        public TimeSpan[] escalaAutorizada { get; set; }
+        public TimeSpan[] horarioFeito { get; set; }
+
+        public HoraExtra() { }
+
+        public HoraExtra(TimeSpan[] escalaTrabalho, TimeSpan[] escalaAutorizada, TimeSpan[] horarioFeito)
+        {
+            this.escalaTrabalho = escalaTrabalho;
+            this.escalaAutorizada = escalaAutorizada;
+            this.horarioFeito = horarioFeito;
+        }
+
+        public TimeSpan CalculaHoraExtra()
+        {
+            TimeSpan horaExtra = TimeSpan.Zero;
+            if (horarioFeito[0] > escalaAutorizada[0] && horarioFeito[0] < escalaTrabalho[0])
+            {
+                horaExtra += escalaTrabalho[0].Subtract(horarioFeito[0]);
+            }
+            if (horarioFeito[1] < escalaAutorizada[1] && horarioFeito[1] > escalaTrabalho[1])
+            {
+                horaExtra += escalaTrabalho[1].Subtract(horarioFeito[1]);
+            }
+            if (horarioFeito[2] > escalaAutorizada[2] && horarioFeito[2] < escalaTrabalho[2])
+            {
+                horaExtra += escalaTrabalho[2].Subtract(horarioFeito[2]);
+            }
+            if (horarioFeito[3] < escalaAutorizada[3] && horarioFeito[3] > escalaTrabalho[3])
+                horaExtra += escalaTrabalho[3].Subtract(horarioFeito[3]);
+            return horaExtra;
+        }
+    }
+}
--- HoraExtra/Program.cs
+++ HoraExtra/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HoraExtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            TimeSpan horaInicial = new TimeSpan(10, 30, 00);
+            TimeSpan horaFinal = new TimeSpan(12, 45, 00);
+
+            TimeSpan diferenca = horaFinal.Subtract(horaInicial);
+            Console.WriteLine(diferenca);
+            */
+
+            TimeSpan[] escalaTrabalho = new TimeSpan[4];
+            escalaTrabalho[0] = new TimeSpan(08, 00, 00);
+            escalaTrabalho[1] = new TimeSpan(12, 00, 00);
+            escalaTrabalho[2] = new TimeSpan(13, 30, 00);
+            escalaTrabalho[3] = new TimeSpan(17, 30, 00);
+
+            TimeSpan[] escalaAutorizada = new TimeSpan[4];
+            escalaAutorizada[0] = new TimeSpan(07, 00, 00);
+            escalaAutorizada[1] = new TimeSpan(12, 00, 00);
+            escalaAutorizada[2] = new TimeSpan(13, 30, 00);
+            escalaAutorizada[3] = new TimeSpan(17, 30, 00);
+
+            TimeSpan[] horarioFeito = new TimeSpan[4];
+            horarioFeito[0] = new TimeSpan(07, 45, 00);
+            horarioFeito[1] = new TimeSpan(12, 00, 00);
+            horarioFeito[2] = new TimeSpan(12, 30, 00);
+            horarioFeito[3] = new TimeSpan(17, 30, 00);
+
+            HoraExtra horaExtra = new HoraExtra(escalaTrabalho, escalaAutorizada, horarioFeito);
+            Console.WriteLine(horaExtra.CalculaHoraExtra());
+        }
+    }
+}
